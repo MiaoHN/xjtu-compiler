64540b7f8c4d4d7765c448cd6387dd302c586b3e
MiaoHN <582418227@qq.com>
2022-05-22 20:57:40 +0800

    ✨ feat(assignment3): 基本完成assignment3

---
+++ assignment3/test/error_test.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+  printf("Hello World\n");
+  $abc
+  return 0;
+}
---
+++ assignment3/test/test.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+  int a = 0;
+  printf("Hello World\n");
+  return 0;
+}
